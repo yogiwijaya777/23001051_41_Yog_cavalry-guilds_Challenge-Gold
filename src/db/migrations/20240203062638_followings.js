919e47da74c12cc81e73915acb800d10f6ed902b
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-02-03 13:27:35 +0700

    knex followings s chema

---
+++ src/db/migrations/20240203062638_followings.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable('followings', function (table) {
+    table.uuid('id').defaultTo(knex.fn.uuid()).primary();
+    table.uuid('userId').references('id').inTable('users').onDelete('CASCADE');
+    table.uuid('followingId').references('id').inTable('users').onDelete('CASCADE');
+    table.timestamp('createdAt').defaultTo(knex.fn.now());
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('followings');
+};
