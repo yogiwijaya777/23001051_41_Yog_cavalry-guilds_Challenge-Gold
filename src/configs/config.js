05e914ff535bff18aa092c5380a832bc1fbe6fb8
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-02-03 14:00:14 +0700

    basic express server

--- src/configs/config.js
+++ src/configs/config.js
@@ -7,6 +7,7 @@ dotenv.config({
 
 module.exports = {
   env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
   development: {
     DB_HOST: process.env.DB_HOST,
     DB_USER: process.env.DB_USER,
@@ -14,3 +15,22 @@ module.exports = {
     DB: process.env.DB,
   },
 };
+
+//  jwt: {
+//     secret: envVars.JWT_SECRET,
+//     accessExpirationMinutes: envVars.JWT_ACCESS_EXPIRATION_MINUTES,
+//     refreshExpirationDays: envVars.JWT_REFRESH_EXPIRATION_DAYS,
+//     resetPasswordExpirationMinutes: envVars.JWT_RESET_PASSWORD_EXPIRATION_MINUTES,
+//     verifyEmailExpirationMinutes: envVars.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES,
+//   },
+//   email: {
+//     smtp: {
+//       host: envVars.SMTP_HOST,
+//       port: envVars.SMTP_PORT,
+//       auth: {
+//         user: envVars.SMTP_USERNAME,
+//         pass: envVars.SMTP_PASSWORD,
+//       },
+//     },
+//     from: envVars.EMAIL_FROM,
+//   },
