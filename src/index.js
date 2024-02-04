cf4b80e0dd6f07ef2baf21030f95a5996309d1a7
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-02-04 12:38:02 +0700

    setup src/index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 const app = require('./app');
 const config = require('./configs/config');
+const logger = require('./configs/logger');
 const knex = require('./db/knex');
 
 let server;
 console.log(knex);
 if (knex) {
-  console.log('Connected to Database');
+  logger.info('Connected to Database');
   server = app.listen(config.port, () => {
-    console.log(`Listening to port ${config.port}`);
+    logger.info(`Listening to port ${config.port}`);
   });
 }
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
