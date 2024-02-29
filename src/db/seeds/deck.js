d411bd9b5ac1935c6fbe3ef78f3cb4c218b8d757
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-02-29 23:12:18 +0700

    decks seed complete

---
+++ src/db/seeds/deck.js
@@ -0,0 +1,119 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('table_name').del();
+  await knex('table_name').insert([
+    {
+      id: 'e98ab0fc-ed0f-47e5-85d0-b667d5907400',
+      name: 'Exosister Regen',
+      description:
+        'Deck "Exosister Regen" adalah deck berbasis Spellcaster yang menekankan pemanggilan kembali monster dari graveyard dan pengendalian permainan melalui kartu sihir dan jebakan. Strateginya adalah memanfaatkan efek pemanggilan kembali dan pengendalian lapangan untuk mengatur tempo permainan dan mendapatkan keunggulan dalam pertarungan.',
+      archetypeId: '6e35aa3d-1573-4b56-99c0-3f2dd4d64475',
+      userId: '',
+    },
+    {
+      id: 'cbfe4713-c553-4d17-b63e-00629e69e5fa',
+      name: 'Exo-Handtrap',
+      description:
+        '"Exosister Handtrap" deck focuses on Spellcaster monsters and handtrap effects to control the game. It utilizes monster revival and field control to gain advantage in battles.',
+      archetypeId: '6e35aa3d-1573-4b56-99c0-3f2dd4d64475',
+      userId: 'f5cc52e7-4bd7-4a50-87f2-da6703ce619c',
+    },
+    {
+      id: '26c50041-6920-4803-9d07-940f0dd927e2',
+      name: 'Exosister Tama',
+      description:
+        'Exosister Tama deck focuses on versatile Spellcaster monsters and trap cards to control the game. It excels at disrupting the opponents strategies while maintaining field presence through monster revival and defensive traps.',
+      archetypeId: '6e35aa3d-1573-4b56-99c0-3f2dd4d64475',
+      userId: 'beb60df8-f3ef-453c-9064-8ae8b459f1a6',
+    },
+    {
+      id: '3fd63be1-e01d-4d38-95e0-34e0549f5b1b',
+      name: 'Exosister Menyala',
+      description:
+        'Exosister Menyala deck emphasizes Spellcaster monsters and flame-based effects to control the game. It utilizes powerful flame-based monsters and spells to disrupt the opponents strategy and gain advantage in battles.',
+      archetypeId: '6e35aa3d-1573-4b56-99c0-3f2dd4d64475',
+      userId: 'a1e98f24-be67-4864-898a-f1b74ce80630',
+    },
+    {
+      id: 'e515685e-8d9a-44f4-be1a-3313eddf4b35',
+      name: 'Exosister Lorem',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius lectus sed justo consequat.',
+      archetypeId: '6e35aa3d-1573-4b56-99c0-3f2dd4d64475',
+      userId: 'a1e98f24-be67-4864-898a-f1b74ce80630',
+    },
+    {
+      id: '9601088c-a94f-4122-b902-cc6e9f6f73c3',
+      name: 'Menyala abangku',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius lectus sed justo consequat.',
+      archetypeId: 'ff15bac3-f615-43ac-a9ed-d2a574a2dac2',
+      userId: 'beb60df8-f3ef-453c-9064-8ae8b459f1a6',
+    },
+    {
+      id: '3248b507-88e0-4a8c-afbb-669960031cc6',
+      name: '',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius lectus sed justo consequat.',
+      archetypeId: '3b8c3177-e4bb-471a-ac40-18d5f5b97a2c',
+      userId: 'fc27a26d-ddfe-4b40-9246-e1e495b71dda',
+    },
+    {
+      id: '8caf65ec-4934-468c-b505-989d9ed0df02',
+      name: 'Deck Lorem',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius lectus sed justo consequat.',
+      archetypeId: '3b8c3177-e4bb-471a-ac40-18d5f5b97a2c',
+      userId: 'b4ea9401-d7de-4966-9044-c7d004377bc2',
+    },
+    {
+      id: '14a79282-5341-44ea-9528-86c51604f47f',
+      name: 'Deck Ipsum',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius lectus sed justo consequat.',
+      archetypeId: '3b8c3177-e4bb-471a-ac40-18d5f5b97a2c',
+      userId: 'f5cc52e7-4bd7-4a50-87f2-da6703ce619c',
+    },
+    {
+      id: '8f8d7a00-3d62-414f-bb65-21b4069417d0',
+      name: 'Dolor',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius lectus sed justo consequat.',
+      archetypeId: '9dfa578e-e6b3-48ff-99ab-972543f4b6c4',
+      userId: 'a1e98f24-be67-4864-898a-f1b74ce80630',
+    },
+    {
+      id: 'c490bd0a-ec85-41b6-8406-8da2af5711e2',
+      name: 'Sit Amet',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius lectus sed justo consequat.',
+      archetypeId: '9dfa578e-e6b3-48ff-99ab-972543f4b6c4',
+      userId: 'fc27a26d-ddfe-4b40-9246-e1e495b71dda',
+    },
+    {
+      id: 'fcf2b1a6-8d39-4b56-a3a9-8c5430a07066',
+      name: 'Consectetur',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius lectus sed justo consequat.',
+      archetypeId: '9dfa578e-e6b3-48ff-99ab-972543f4b6c4',
+      userId: 'b4ea9401-d7de-4966-9044-c7d004377bc2',
+    },
+    {
+      id: '8e435ae3-69bd-4277-b0a1-00b10ed278cd',
+      name: 'adipiscing elit. Suspendisse varius lectus sed justo',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius lectus sed justo consequat.',
+      archetypeId: '9dfa578e-e6b3-48ff-99ab-972543f4b6c4',
+      userId: 'beb60df8-f3ef-453c-9064-8ae8b459f1a6',
+    },
+    {
+      id: 'aea98896-b75d-467a-8fb1-24df5e0be41a',
+      name: 'Deck Ipsum 2',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius lectus sed justo consequat.',
+      archetypeId: '760efab5-a405-4f96-b31e-98ea6e0b2aa1',
+      userId: 'beb60df8-f3ef-453c-9064-8ae8b459f1a6',
+    },
+    {
+      id: '87634e63-35dd-4a44-83ef-ff87bfc082cf',
+      name: 'MECHA',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius lectus sed justo consequat.',
+      archetypeId: '760efab5-a405-4f96-b31e-98ea6e0b2aa1',
+      userId: 'fc27a26d-ddfe-4b40-9246-e1e495b71dda',
+    },
+  ]);
+};
