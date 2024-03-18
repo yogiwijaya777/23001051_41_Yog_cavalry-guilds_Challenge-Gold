42590f40175e25addb7be0001615ff5dfc97e490
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-03-18 19:05:51 +0700

    fix bug on favorites deck and splitting component

--- client/src/components/SingleDeck.jsx
+++ client/src/components/SingleDeck.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router';
 import useFetchData from '../utils/useFetchData';
 import Loading from './Loading';
@@ -8,70 +7,14 @@ import { useAuth } from '../contexts/AuthContext';
 import addFlAttachmentToUrl from '../utils/addFlAttachmentToUrl';
 import UpdateDeck from './UpdateDeck';
 import axios from 'axios';
+import DeckFavorites from './DeckFavorites';
 
 function SingleDeck() {
   const { user, token } = useAuth();
-  const [isFavorited, setIsFavorited] = useState(false);
-  const [totalFavorites, setTotalFavorites] = useState(0);
-  const [favorite, setFavorite] = useState(null);
   const { pathname } = useLocation();
   const deckId = pathname.split('/')[2];
 
   const { data: deck, loading, error } = useFetchData(`${process.env.REACT_APP_API_URL}/decks/${deckId}`);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (user) {
-          const response = await axios.get(`${process.env.REACT_APP_API_URL}/favorite-decks/${deckId}/${user.id}`, {
-            headers: {
-              Authorization: `Bearer ${token.access.token}`,
-            },
-          });
-          if (response.status === 200) {
-            setIsFavorited(true);
-            setFavorite(response.data.data);
-          }
-        }
-      } catch (error) {
-        error.response.status === 404 ? setIsFavorited(false) : alert(error.response.data.message);
-      }
-    };
-    fetchData();
-  });
-
-  useEffect(() => {
-    if (deck) setTotalFavorites(deck.totalFavorites);
-  }, [deck]);
-
-  const handlerFavorite = async () => {
-    try {
-      if (user) {
-        if (isFavorited) {
-          await axios.delete(`${process.env.REACT_APP_API_URL}/favorite-decks/${favorite.id}`, {
-            headers: {
-              Authorization: `Bearer ${token.access.token}`,
-            },
-          });
-          setTotalFavorites(totalFavorites - 1);
-        } else {
-          await axios.post(
-            `${process.env.REACT_APP_API_URL}/favorite-decks`,
-            { deckId },
-            {
-              headers: {
-                Authorization: `Bearer ${token.access.token}`,
-              },
-            }
-          );
-          setTotalFavorites(totalFavorites + 1);
-        }
-        setIsFavorited(!isFavorited);
-      }
-    } catch (error) {
-      alert(error.response.data.message);
-    }
-  };
 
   return loading ? (
     <Loading />
@@ -101,14 +44,7 @@ function SingleDeck() {
           <img className="img-fluid" src={deck.imageUrl} alt="Deck" />
         </div>
         <div className="container deck--detail col-6">
-          <p className="">
-            <i
-              style={{ cursor: 'pointer' }}
-              className={`button bi bi-star${isFavorited ? '-fill' : ''}`}
-              onClick={handlerFavorite}
-            />
-            <span className="user-select-none">&nbsp;{totalFavorites}</span>
-          </p>
+          <DeckFavorites deck={deck} user={user} token={token} />
           <p>Archetype: {deck.archetypeName}</p>
           <br />
           <p>Description :</p>
