2f9d329b429b6782070075adffee0ffd1205fc72
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-03-18 17:51:46 +0700

    GET FAVORITED DECK BY DECK AND USER SENDING DATA

--- src/controllers/api-v1/favoriteDeck.controller.js
+++ src/controllers/api-v1/favoriteDeck.controller.js
@@ -44,12 +44,12 @@ const getFollowersDeckByDeck = catchAsync(async (req, res) => {
 });
 
 const getbyDeckAndUser = catchAsync(async (req, res) => {
-  const results = await favoriteDeckService.getbyDeckAndUser(req.params.deckId, req.params.userId);
+  const result = await favoriteDeckService.getbyDeckAndUser(req.params.deckId, req.params.userId);
 
   res.status(httpStatus.OK).json({
     status: httpStatus.OK,
     message: 'Get Favorited Decks By User Success',
-    data: results.favoriteDecks,
+    data: result,
   });
 });
 
