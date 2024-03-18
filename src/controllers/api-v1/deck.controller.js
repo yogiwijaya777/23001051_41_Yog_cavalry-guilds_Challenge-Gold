731d26a6a00122e8fa87bc0642b2328e29b3e08f
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-03-18 08:30:26 +0700

    complete cloudinary setup at backend

--- src/controllers/api-v1/deck.controller.js
+++ src/controllers/api-v1/deck.controller.js
@@ -5,11 +5,11 @@ const ApiError = require('../../utils/ApiError');
 const cloudinary = require('cloudinary').v2;
 const fs = require('fs');
 
-const uploadCloudinary = async (name, image) => {
+const uploadCloudinary = async (deckName, userId, image) => {
   try {
     const result = await cloudinary.uploader.upload(image.tempFilePath, {
-      use_filename: false,
-      public_id: name,
+      public_id: `${deckName}-${userId}`,
+      overwrite: true,
       folder: 'cavalry/decks',
       resource_type: 'image',
       image_format: 'png',
@@ -30,7 +30,7 @@ const create = catchAsync(async (req, res) => {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Image is too large');
   }
 
-  const imgUrl = await uploadCloudinary(req.body.name, image);
+  const imgUrl = await uploadCloudinary(req.body.name, req.user.id, image);
 
   req.body.imageUrl = imgUrl;
 
@@ -56,13 +56,15 @@ const getById = catchAsync(async (req, res) => {
 const update = catchAsync(async (req, res) => {
   const image = req.files.image;
 
-  if (image.truncated) {
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Image is too large');
+  if (image) {
+    if (image.truncated) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'Image is too large');
+    }
+
+    const imgUrl = await uploadCloudinary(req.body.name, req.user.id, image);
+
+    req.body.imageUrl = imgUrl;
   }
-
-  const imgUrl = await uploadCloudinary(req.body.name, image);
-
-  req.body.imageUrl = imgUrl;
 
   const deckUpdated = await deckService.update(req.user, req.params.deckId, req.body);
 
