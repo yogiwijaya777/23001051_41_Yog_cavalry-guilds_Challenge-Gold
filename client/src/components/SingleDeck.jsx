3742bf96fcc440ce709a63689f52f3725257492d
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-03-18 17:52:08 +0700

    FINALLY FAVORITE DECK FUNCTIONAL COMPLETE

--- client/src/components/SingleDeck.jsx
+++ client/src/components/SingleDeck.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router';
 import useFetchData from '../utils/useFetchData';
 import Loading from './Loading';
@@ -7,19 +7,71 @@ import DeleteDeck from './DeleteDeck';
 import { useAuth } from '../contexts/AuthContext';
 import addFlAttachmentToUrl from '../utils/addFlAttachmentToUrl';
 import UpdateDeck from './UpdateDeck';
+import axios from 'axios';
 
 function SingleDeck() {
-  const [isUpdate, setIsUpdate] = useState(false);
   const { user, token } = useAuth();
-
-  if (isUpdate) {
-    setIsUpdate(false);
-  }
-
+  const [isFavorited, setIsFavorited] = useState(false);
+  const [totalFavorites, setTotalFavorites] = useState(0);
+  const [favorite, setFavorite] = useState(null);
   const { pathname } = useLocation();
   const deckId = pathname.split('/')[2];
 
   const { data: deck, loading, error } = useFetchData(`${process.env.REACT_APP_API_URL}/decks/${deckId}`);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (user) {
+          const response = await axios.get(`${process.env.REACT_APP_API_URL}/favorite-decks/${deckId}/${user.id}`, {
+            headers: {
+              Authorization: `Bearer ${token.access.token}`,
+            },
+          });
+          if (response.status === 200) {
+            setIsFavorited(true);
+            setFavorite(response.data.data);
+          }
+        }
+      } catch (error) {
+        error.response.status === 404 ? setIsFavorited(false) : alert(error.response.data.message);
+      }
+    };
+    fetchData();
+  });
+
+  useEffect(() => {
+    if (deck) setTotalFavorites(deck.totalFavorites);
+  }, [deck]);
+
+  const handlerFavorite = async () => {
+    try {
+      if (user) {
+        if (isFavorited) {
+          await axios.delete(`${process.env.REACT_APP_API_URL}/favorite-decks/${favorite.id}`, {
+            headers: {
+              Authorization: `Bearer ${token.access.token}`,
+            },
+          });
+          setTotalFavorites(totalFavorites - 1);
+        } else {
+          await axios.post(
+            `${process.env.REACT_APP_API_URL}/favorite-decks`,
+            { deckId },
+            {
+              headers: {
+                Authorization: `Bearer ${token.access.token}`,
+              },
+            }
+          );
+          setTotalFavorites(totalFavorites + 1);
+        }
+        setIsFavorited(!isFavorited);
+      }
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
 
   return loading ? (
     <Loading />
@@ -49,18 +101,13 @@ function SingleDeck() {
           <img className="img-fluid" src={deck.imageUrl} alt="Deck" />
         </div>
         <div className="container deck--detail col-6">
-          <p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-star"
-              viewBox="0 0 16 16"
-            >
-              <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z" />
-            </svg>
-            {deck.totalFavorites}
+          <p className="">
+            <i
+              style={{ cursor: 'pointer' }}
+              className={`button bi bi-star${isFavorited ? '-fill' : ''}`}
+              onClick={handlerFavorite}
+            />
+            <span className="user-select-none">&nbsp;{totalFavorites}</span>
           </p>
           <p>Archetype: {deck.archetypeName}</p>
           <br />
