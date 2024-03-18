7d363f69929147eacd6215ea2f9c218b85ab135a
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-03-18 08:43:23 +0700

    remove console log at addAttachment utils

--- client/src/utils/addFlAttachmentToUrl.js
+++ client/src/utils/addFlAttachmentToUrl.js
@@ -3,7 +3,3 @@ export default function addFlAttachmentToUrl(url) {
 
   return updatedUrl;
 }
-
-const originalUrl = 'https://res.cloudinary.com/dmf8l6plb/image/upload/v1710723847/cavalry/decks/Branded%20Kuriboh3.png';
-const updatedUrl = addFlAttachmentToUrl(originalUrl);
-console.log(updatedUrl);
