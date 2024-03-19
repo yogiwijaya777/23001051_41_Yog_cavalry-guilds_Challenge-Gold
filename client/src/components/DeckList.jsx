f45431b6846e9c75df3d1d344ba2323e997fba3a
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-03-19 13:24:39 +0700

    styling decks card

--- client/src/components/DeckList.jsx
+++ client/src/components/DeckList.jsx
@@ -1,23 +1,29 @@
+import { Link } from 'react-router-dom';
 import '../css/DeckList.css';
 
 function DeckList({ deck }) {
   return (
-    <div className="card mb-2 ms-4 mt-3 col-lg-2 col-md-4 col-sm-6 bg-white" key={deck.id}>
-      <div>
-        <img src={`/img/archetypes/${deck.archetypeName}.jpg`} className="img-thumbnail img-fluid" alt="..." />
-      </div>
-      <div className="card-body text-center me-4">
-        <a
-          className="icon-link icon-link-hover"
-          style={{ '--bs-icon-link-transform': 'translate3d(0, -.125rem, 0)' }}
-          href={`/decks/${deck.id}`}
-        >
-          <svg className="bi" aria-hidden="true"></svg>
-          {deck.name}
-        </a>
+    <div className="col ">
+      <div className="card bg-dark-subtle" key={deck.id}>
+        <div>
+          <img src={`${deck.imageUrl}`} className="card-img-top" alt="..." />
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">{deck.name}</h5>
+          <p className="card-text">
+            {deck.description.length > 70 ? deck.description.substring(0, 70) + '...' : deck.description}
+          </p>
+        </div>
+        <div class="mb-5 d-flex justify-content-around">
+          <h3>{deck.archetypeName}</h3>
+          <Link to={`/decks/${deck.id}`} className="btn btn-primary me-2">
+            See Details...
+          </Link>
+        </div>
       </div>
     </div>
   );
 }
 
 export default DeckList;
+// mb-2 ms-4 mt-3 col-lg-2 col-md-4 col-sm-6 bg-white
