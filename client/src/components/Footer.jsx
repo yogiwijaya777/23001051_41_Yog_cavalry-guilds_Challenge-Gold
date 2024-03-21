19c968833f9533cac4f997c7d43559dc19be6d1a
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-03-21 13:41:30 +0700

    Push Footers

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,48 +1,46 @@
-import './Footer.css'
-import twitch from '../assets/twitch.png'
-import fb from '../assets/fb.png'
-import yt from '../assets/yt.png'
-import twitter from '../assets/twitter.png'
-const  Footer = () => {
-    return (
-        <div className="footer">
-            <ul className="social-icon">
-                <li>
-                    <a href="#">
-                        <img src={twitch} alt=''/>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <img src={fb} alt=''/>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <img src={yt} alt=''/>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <img src={twitter} alt=''/>
-                    </a>
-                </li>
-            </ul>
+import '../css/Footer.css';
 
-            <hr></hr>
-            <p>@2024 Calvary Guilds</p>
-            <ul className="menu">
-                <li>
-                    <a href="#">Contact</a>
-                    <a href="/">Home</a>
-                    <a href="/topdeck">Top Decks</a>
-                    <a href="/about">About</a>
-                </li>
-            </ul>
-        </div>
-
-        
-    )
-}
+const Footer = () => {
+  return (
+    <footer className="footer ">
+      <div className="btn-group">
+        <button type="button" className="btn btn-outline-secondary">
+          <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+            <i className="bi bi-facebook"></i>
+          </a>
+          <span className="visually-hidden">Button</span>
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
+            <i className="bi bi-twitter-x"></i>
+          </a>
+          <span className="visually-hidden">Button</span>
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          <a href="https://www.discord.com" target="_blank" rel="noreferrer">
+            <i className="bi bi-discord"></i>
+          </a>
+          <span className="visually-hidden">Button</span>
+        </button>
+        <button type="button" className="btn btn-outline-secondary">
+          <a href="https://www.twitch.tv" target="_blank" rel="noreferrer">
+            <i className="bi bi-twitch"></i>
+          </a>
+          <span className="visually-hidden">Button</span>
+        </button>
+      </div>
+      <hr></hr>
+      <p>@2024 Calvary Guilds</p>
+      <ul className="menu">
+        <li>
+          <a href="#contact">Contact</a>
+          <a href="/">Home</a>
+          <a href="/top-decks">Top Decks</a>
+          <a href="/#about">About</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
 
 export default Footer;
