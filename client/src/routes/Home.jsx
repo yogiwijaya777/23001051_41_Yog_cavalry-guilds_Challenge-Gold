8f9a184e18201a3ee48bbea4f87c79cd70339e30
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-03-23 23:46:34 +0700

    Our Services Section

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,12 +1,14 @@
 import '../css/Home.css';
 import Carousel from '../components/Carousel';
 import About from '../components/About';
+import Services from '../components/Services';
 
 function Home() {
   return (
     <div>
       <Carousel />
       <About />
+      <Services />
     </div>
   );
 }
