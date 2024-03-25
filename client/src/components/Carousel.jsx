46cd37f509ff789cf32dec6381a6c431ac7913d9
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-03-25 20:01:07 +0700

    Carouse Component test

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,6 +1,10 @@
 function Carousel() {
   return (
-    <div id="carouselExampleCaptions" className="carousel slide ">
+    <div
+      id="carouselExampleCaptions"
+      data-testid="carousel"
+      className="carousel slide "
+    >
       <div className="carousel-indicators">
         <button
           type="button"
@@ -10,12 +14,26 @@ function Carousel() {
           aria-current="true"
           aria-label="Slide 1"
         ></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 2"></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="2"
+          aria-label="Slide 2"
+        ></button>
       </div>
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <img src="/img/carousels/Rescue Ace.png" alt="Rescue Ace" className="d-block w-100" />{' '}
+          <img
+            src="/img/carousels/Rescue Ace.png"
+            alt="Rescue Ace"
+            className="d-block w-100"
+          />{" "}
           <div className="carousel-caption ">
             <h1>Do You Need at Building Your Deck?</h1>
             <p>We have so much to offer for free!</p>
@@ -27,7 +45,11 @@ function Carousel() {
           </div>
         </div>
         <div className="carousel-item">
-          <img src="/img/carousels/Exosister.jpg" alt="mikailis" className="d-block w-100" />
+          <img
+            src="/img/carousels/Exosister.jpg"
+            alt="mikailis"
+            className="d-block w-100"
+          />
           <div className="carousel-caption ">
             <h1>Are You Want to be a Champion?</h1>
             <p>We are the best place for you!</p>
@@ -39,7 +61,11 @@ function Carousel() {
           </div>
         </div>
         <div className="carousel-item">
-          <img src="/img/carousels/Mannadium.jpg" alt="Mannadium" className="d-block w-100" />
+          <img
+            src="/img/carousels/Mannadium.jpg"
+            alt="Mannadium"
+            className="d-block w-100"
+          />
           <div className="carousel-caption  ">
             <h1>Ready for The Next Duel?</h1>
             <p>It's time to ddduel!</p>
@@ -51,11 +77,21 @@ function Carousel() {
           </div>
         </div>
       </div>
-      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Previous</span>
       </button>
-      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
         <span className="carousel-control-next-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Next</span>
       </button>
