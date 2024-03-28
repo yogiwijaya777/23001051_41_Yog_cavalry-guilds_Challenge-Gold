764fea4e76da478a04cb53f5631bd7ec953fa942
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-03-28 12:46:55 +0700

    Deck List Component Testing complete

---
+++ client/src/routes/Deck.jsx
@@ -0,0 +1,12 @@
+import SingleDeck from "./SingleDeck";
+
+function Deck() {
+  return (
+    <div>
+      <title>Cavalry : View Deck</title>;
+      <SingleDeck />
+    </div>
+  );
+}
+
+export default Deck;
