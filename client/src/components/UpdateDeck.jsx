b59d95dc8c6307091d7763c26dc2992b4a0e8ae6
yogiwijaya777 <yogiwijaya115@gmail.com>
2024-04-06 11:18:28 +0700

    Express Rate Limiter for API REQUEST

--- client/src/components/UpdateDeck.jsx
+++ client/src/components/UpdateDeck.jsx
@@ -142,6 +142,7 @@ function UpdateDeck({ token, deckId }) {
                 <Form.Control
                   id="file-input"
                   type="file"
+                  accept="image/*"
                   onChange={handleFileChange}
                 />
               </Form.Group>
